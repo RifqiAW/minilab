413afe603df38952f925354fb5acf1367ed8c5ea
RifqiAW <RifqiAW@users.noreply.github.com>
2022-03-30 09:55:19 +0700

    Add order domain
    nambahin order entity

--- src/main/java/com/eksadsupport/minilab/domain/Order.java
+++ src/main/java/com/eksadsupport/minilab/domain/Order.java
@@ -1,5 +1,6 @@
 package com.eksadsupport.minilab.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -12,4 +13,37 @@ public class Order {
     @Id
     @Size(max = 50)
     private String orderId;
+
+    @Column(name = "minimum_payment")
+    private double minimumPayment;
+
+    @Column(name = "total_value")
+    private double totalValue;
+
+    @Column(name = "order_value")
+    private double orderValue;
+
+    @Column(name = "offtheroad_value")
+    private double offtheroadValue;
+
+    @Column(name = "order_total_discount")
+    private double orderTotalDiscount;
+
+    @Column(name = "ppn")
+    private double ppn;
+
+    @Column(name = "plat_nomor")
+    private String platNomor;
+
+    @Column(name = "nomor_mesin")
+    private String nomorMesin;
+
+    @Column(name = "is_leasin")
+    private String isLeasing;
+
+    @Column(name = "payment_status")
+    private String paymentStatus;
+
+    @Column(name = "unit_status")
+    private String unitStatus;
 }
