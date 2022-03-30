89201c75422ed6b70ea2be5804e9d20b3e9d9f5a
RifqiAW <RifqiAW@users.noreply.github.com>
2022-03-30 11:21:48 +0700

    Edit order
    edit order

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,3 @@
 insert into mst_sales(sales_id, sales_name, supervisor_id, sales_gender, sales_email, sales_status) values ('M1', 'Offline', NULL, 'GTLK', 'asd', 'INACTIVE');
+
+insert into trx_order(order_id, unit_code, dealer_code, sales_id, customer_id, minimum_payment, total_value, order_value, offtheroad_value, order_total_discount, ppn, plat_nomor, nomor_mesin, nomor_rangka, is_leasing, payment_status, unit_status) values ('M1', 'unitcode', 'dealercode', 'M1', 'customerid', 5.0, 3.0, 2.0, 1.0, 6.0, 'PL4T', '11', 'NO', 'FULLY_PAID', 'UNIT_RECEIVED');
