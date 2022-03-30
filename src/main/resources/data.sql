bf1b995772028aea6fd2fac57f58d5d834d8925a
DESKTOP-TP0HQV7\Darma <jaeger961@gmail.com>
2022-03-30 11:20:14 +0700

    Add Domain Ppn, Edit Schema.sql & Data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,3 @@
 insert into mst_sales(sales_id, sales_name, supervisor_id, sales_gender, sales_email, sales_status) values ('M1', 'Offline', NULL, 'GTLK', 'asd', 'INACTIVE');
+
+insert into mst_ppn (ppn_id, description, dealer_code, effective_start_date, effective_end_date, ppn_rate, ppn_rate_previous, ppn_status) values ('P1', 'Ppn ke 1', '100012', '14-02-2022 16:00', '18-02-2022 19:30', 11.5, 10, 'INACTIVE');
